b096f8e84f8b81a826642c1660a855af5fdf384f
User <User@User-PC>
2020-12-04 18:38:55 +0530

    WH-998 Next Commit

--- src/main/java/com/github/aadil/runner/MessageRunner.java
+++ src/main/java/com/github/aadil/runner/MessageRunner.java
@@ -9,7 +9,8 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Welcome To Spring Boot App!!");
-	
+		System.out.println("Welcome To Spring Boot App!!");
+
 	}
 
 }
