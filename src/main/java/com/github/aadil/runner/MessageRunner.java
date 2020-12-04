0515ebdffde8b1c203fb556808fcc96d5e685b88
User <User@User-PC>
2020-12-04 18:56:20 +0530

    WH-4125 UPDATE RUNNER WITH LOG4J USING  SLF4J

--- src/main/java/com/github/aadil/runner/MessageRunner.java
+++ src/main/java/com/github/aadil/runner/MessageRunner.java
@@ -1,16 +1,25 @@
 package com.github.aadil.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log= LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
 		int a=10;
+		try {
+		log.info("STARTED");
 		System.out.println("Welcome To Spring Boot App!!"+a);
-
+		}
+		catch(Exception e) {
+		e.printStackTrace();	
+		}	log.info("END");
+		}
 	}
 
-}
+
